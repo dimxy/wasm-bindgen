3a51a925e65db04ab9785361c9239a22a229f468
dimxy <dimxy@komodoplatform.com>
2024-10-23 17:41:28 +0500

    try to call simple wasm from workers and window with shared var

--- examples/synchronous-instantiation/src/lib.rs
+++ examples/synchronous-instantiation/src/lib.rs
@@ -1,4 +1,12 @@
+//! Try to lock from workers
+
 use wasm_bindgen::prelude::*;
+use lazy_static::lazy_static;
+
+lazy_static!{
+    //static ref MY_NUM: tokio::sync::Mutex<u32> = tokio::sync::Mutex::new(4); // async mutex
+    static ref MY_NUM: std::sync::Mutex<u32> = std::sync::Mutex::new(12); // std sync Mutex, also works
+}
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,5 +16,18 @@ extern "C" {
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    log(&format!("Hello, {}!", name));
+    //let mut num = MY_NUM.lock().await;
+    //tokio::time::sleep().await;
+    log(&format!("Hello, {}! thread_id={:?} waiting for lock..", name, std::thread::current().id()));
+    let mut num = MY_NUM.lock().unwrap();
+    let mut f = 1.111;
+    for i in 0..100000000 { 
+        f = 1.1111 / 7.77777 * f; 
+        if i % 10000000 == 0 {
+            log(&format!("Hello, {}! doing work..", name));
+        }
+    } // do some work
+    *num = *num + 1;
+    //let num = 0;
+    log(&format!("Hello, {}! num={}", name, num));
 }
--- examples/synchronous-instantiation/worker.js
+++ examples/synchronous-instantiation/worker.js
@@ -1,6 +1,8 @@
-import * as wasm from "./pkg/synchronous_instantiation.js";
+//import * as wasm from "./pkg/synchronous_instantiation.js";
+self.wasm = await import("./pkg/synchronous_instantiation.js")
 
-self.onmessage = ({ data: bytes }) => {
+self.onmessage = async ({ data: bytes }) => {
+
   /**
    * When we receive the bytes as an `ArrayBuffer` we can use that to
    * synchronously initialize the module as opposed to asynchronously
@@ -8,11 +10,14 @@ self.onmessage = ({ data: bytes }) => {
    * `new WebAssembly.Module()` and `new WebAssembly.Instance()`.
    */
   wasm.initSync({ module: bytes });
+  console.log("worker wasm.init'ed");
 
   /**
    * Once initialized we can call our exported `greet()` functions.
    */
-  wasm.greet("Dominic");
+  //await new Promise(r => setTimeout(r, 2000));
+  console.log("worker calling wasm.greet");
+  wasm.greet("in-worker");
 };
 
 /**
@@ -20,4 +25,8 @@ self.onmessage = ({ data: bytes }) => {
  * for the WebAssembly module. Once fetched it will send the bytes back via
  * a `postMessage` (see above).
  */
+
+console.log("worker started");
+//await new Promise(r => setTimeout(r, 2000)); // wait for 2 sec for workers to start
+console.log("posting FETCH_WASM");
 self.postMessage({ type: "FETCH_WASM" });
